5c9d31e16c77534bd0d8815cc5c95b4475c66167
webrohit27 <158483773+webrohit27@users.noreply.github.com>
2024-02-04 17:30:58 +0000

    map done

--- Hof/map.js
+++ Hof/map.js
@@ -6,4 +6,25 @@ const newNums = myNums.filter( (num) => {
     return num > 4
 } );
 
-console.log(newNums);
+//console.log(newNums);
+
+const num = [15, 20, 25, 30,40];
+
+const num1 = num.map( (num) => { return num + 10} );
+
+//console.log(num1);
+//[ 25, 30, 35, 40, 50 ]
+
+
+
+// chaining
+
+const digit = [1, 5, 9, 3, 4];
+
+const newDigit  = digit
+                  .map( (digit) => digit * 10 )
+                  .map( (digit) => digit + 10 )
+                 .filter ( (digit) => digit > 40);
+
+                  console.log(newDigit);
+                  // [ 60, 100, 50 ]
---
+++ Hof/reduce.js
@@ -0,0 +1,2 @@
+// Reduce()
+  The reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value.
