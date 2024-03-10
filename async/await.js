45aeee94cda0bd91a5a6ef77a6d82c5256a36da2
rohitrodge <webrohit27@gmail.com>
2024-03-10 22:43:05 +0530

    update await

--- async/await.js
+++ async/await.js
@@ -2,18 +2,28 @@
 // Async is keyword which is used as async function.
 // Async is always return promises.
 
-const p = new Promise((resolve, reject) => {
+const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('promise is resolved')
     }, 10000);
+}); 
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('promise is resolved')
+    }, 5000);
 }); 
 
 async function handlePromise() {
     console.log('Hello World');
 
     // js engine is waiting for promise 
-    const val = await p;
+    const val = await p1;
     console.log('bhokat ja');
     console.log(val);
+
+    const val2 = await p2;
+    console.log('bhokat ja');
+    console.log(val2);
 }
-handlePromise();
+handlePromise(); 
