7c177af156104efb9a6096e4d974f642fad0d71d
rohitrodge <webrohit27@gmail.com>
2024-03-12 22:32:34 +0530

    commit cube

--- practice/leaf.js
+++ practice/leaf.js
@@ -7,14 +7,3 @@ if (year % 4 === 0) {
 }
 
 
-// Initialize a variable to hold the sum
-let sum = 0;
-
-// Use a for loop to iterate through the first 10 natural numbers
-for (let i = 1; i <= 10; i++) {
-    // Add the current number to the sum
-    sum += i;
-}
-
-// Print the sum
-console.log(sum);
---
+++ practice/while.js
@@ -0,0 +1,8 @@
+let i = 0;
+
+let cube ;
+while (i<=6) {
+    cube = i*i*i;
+    console.log(cube);
+    i++;
+}
